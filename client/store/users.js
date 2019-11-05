dcbb960b80f4137403b4c2e4216627170f7b1fa7
Dan Chun <dchun016@gmail.com>
2019-11-05 12:44:21 -0600

    briefly wrote up getProducts and getUsers redux

---
+++ client/store/users.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const GET_USERS = 'GET_USERS'
+
+const getUsers = users => ({type: GET_USERS, users})
+
+export const getUsersThunk = () => async dispatch => {
+  try {
+    const response = await axios.get('/api/users')
+    const users = response.data
+    dispatch(getUsers(users))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const usersReducer = (state = [], action) => {
+  switch (action.type) {
+    case GET_USERS:
+      return action.users
+    default:
+      return state
+  }
+}
+
+export default usersReducer
