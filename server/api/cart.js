6d50f4adc8d12df6c72d20900dccb17b8afbbe51
Chris Kang <christopha.kang@gmail.com>
2019-11-07 12:36:08 -0600

    minor updates to routes

---
+++ server/api/cart.js
@@ -0,0 +1,39 @@
+//worry about importing later
+const router = require('express').Router()
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+const {Order, PricingHistory, Product} = require('../db/models')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const cart = await Order.findOne({
+      where: {
+        status: 'pending',
+        sessionId: req.session.id
+      },
+      include: [
+        {
+          model: Product,
+          include: [
+            {
+              model: PricingHistory,
+              where: {
+                effectiveDate: {
+                  [Op.lt]: new Date()
+                }
+              },
+              order: [['effectiveDate', 'DESC']],
+              limit: 1
+            }
+          ]
+        }
+      ]
+    })
+    if (!cart) res.json({})
+    else res.json(cart)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
