2e943e89ed88e0f389fe1cda1df3722a9c388e19
Henry Griffith <hgriffithdev@gmail.com>
2019-11-08 11:17:50 -0600

    completed classes, worked on randomizing All Products so that not bullked up by class

--- script/seed.js
+++ script/seed.js
@@ -12,16 +12,151 @@ const {
 } = require('../server/db/models/index')
 const faker = require('faker')
 
-const images = {
-  sushi: 'http://lorempixel.com/640/480/food/8'
+const randomNum = num => {
+  return Math.floor(Math.random() * num) + 1
 }
-const image1 = faker.image.food()
-const image2 = faker.image.food()
+const randomAdj = adjs => {
+  return adjs[this.randomNum(adjs.length - 1)]
+}
+const mealInd = [1, 2, 3, 4, 8, 9, 11, 13]
+// FOOD STUFF `http://lorempixel.com/256/256/food/${foodInd}` //random of foodInd.length
+const dinnerAdj = [
+  'Hearty ',
+  'Delicous ',
+  'Healthy ',
+  'Gluten-free ',
+  '1 day old ',
+  '2 day old ',
+  '3 day old ',
+  'Expired ',
+  'Ready-made ',
+  'Half-eaten',
+  'Quick and Easy ',
+  "Yesterday's ",
+  'Average ',
+  'Disgusting ',
+  'Leftover ',
+  'Moldy ',
+  'Surprisingly tasty '
+]
+const transInd = [1, 2, 4, 5, 6, 8, 9, 15]
+const transportAdj = [
+  'Deluxe ',
+  'State of the Art ',
+  'Fashionable ',
+  'Double-decker ',
+  'Really nice ',
+  'Yung ',
+  'Fast ',
+  'Slow ',
+  'Underwhelming ',
+  'Small ',
+  'Large ',
+  'Electric ',
+  'Hybrid',
+  'Sleek ',
+  'Armored ',
+  'Military-grade ',
+  'Rusty ',
+  'Model G61',
+  'Model M49',
+  'Model RT0',
+  'Model BV3',
+  'Model JH8',
+  'Model LK0',
+  'Model AS1',
+  'Model NY6',
+  'Model LK9',
+  'Model RE5',
+  'Model PO9',
+  'Model WS2',
+  'Model GF3'
+]
+const anmlAdj = [
+  'Fluffy ',
+  'Genuine ',
+  'Recently groomed ',
+  'Recently rescued ',
+  'Hirsute ',
+  'White ',
+  'Orange ',
+  'Loving ',
+  'Good ',
+  'Cool '
+]
 
-console.log(image1)
-console.log(image2)
-console.log(image1)
-console.log(image2)
+const techInd = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 19, 20]
+const techAdj = [
+  'Ancient ',
+  'Cybernetic',
+  'State of the Art ',
+  'Really sick ',
+  'Dope ',
+  'Really old ',
+  'Cool smart ',
+  'Great awesome ',
+  'Yung ',
+  'Unbelievably functional ',
+  'New ',
+  'High-quality ',
+  'Wearable ',
+  'Educational '
+]
+
+class Food {
+  constructor() {
+    //eventually take in a typestring
+    //typestring to be meal, snack, dinner, lunch, breakfast
+    this.name = randomAdj(dinnerAdj) + 'meal'
+    this.description = '' //bacon text for food? yeee
+    this.quantity = randomNum(20)
+    this.imageUrl = [
+      `http://lorempixel.com/256/256/food/${
+        mealInd[randomNum(mealInd.length - 1)]
+      }`
+    ]
+  }
+}
+class Transport {
+  constructor() {
+    //typestring
+    //type to be carrier, vehicle, portability, fancy alternative to walking
+    this.name = randomAdj(transportAdj) + 'vehicle'
+    this.description =
+      'Great way of getting around. Not too expensive and wonderfully made'
+    this.quantity = randomNum(5)
+    this.imageUrl = [
+      `http://lorempixel.com/256/256/food/${
+        transInd[randomNum(transInd.length - 1)]
+      }`
+    ]
+  }
+}
+class Animal {
+  constructor(typeString) {
+    //typestring
+    //type to be dude, small guy, bigboi, pal, lil guy
+    this.name = randomAdj(anmlAdj) + 'pal'
+    this.description = ''
+    this.quantity = 1
+    this.imageUrl = [`http://lorempixel.com/256/256/food/${randomNum(20)}`]
+  }
+}
+class Tech {
+  constructor(typeString) {
+    //typestring
+    //type to be tech, technology, piece of science, machinery, piece of tech
+    this.name = randomAdj(techAdj) + 'technology'
+    this.description =
+      'Great way of getting around. Not too expensive and wonderfully made'
+    this.quantity = randomNum(5)
+    this.imageUrl = [
+      `http://lorempixel.com/256/256/food/${
+        techInd[randomNum(techInd.length - 1)]
+      }`
+    ]
+  }
+}
 
 async function seed() {
   await db.sync({force: true})
@@ -81,7 +216,12 @@ async function seed() {
       postalCode: '60666'
     })
   ])
-  const categories = await Category.create({name: 'the only category'})
+  const categories = await Promise.all([
+    Category.create({name: 'Food'}),
+    Category.create({name: 'Transport'}),
+    Category.create({name: 'Animals'}),
+    Category.create({name: 'Technology'})
+  ])
   const pricingHistories = await Promise.all([
     PricingHistory.create({
       price: 50,
