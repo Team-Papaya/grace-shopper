3192e49e866fd0de1b3126f9b8fc58f7588c34fe
Robert Bacon <rb40345@gmail.com>
2019-11-08 12:17:00 -0600

    added purchase profile api handling

---
+++ server/api/checkout.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const Sequelize = require('sequelize')
+//const Op = Sequelize.Op
+const {Order, PurchaseProfile} = require('../db/models')
+
+router.put('/:orderId/newProfile', async (req, res, next) => {
+  const profile = await PurchaseProfile.create(req.body)
+  if (req.session.userId) PurchaseProfile.addUser(req.session.userId)
+  Order.findByPk(req.params.orderId)
+    .then(dbRes => dbRes.setPurchaseProfile(profile))
+    .then(() => res.sendStatus(204))
+    .catch(err => next(err))
+})
+router.post('/:orderId/existingProfile/:profileId', (req, res, next) => {
+  Order.findByPk(req.params.orderId)
+    .then(ord => ord.setPurchaseProfile(req.params.profileId))
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
