447d5f7c08ffeabc5b1b1731b053a7bb4953ccc1
Henry Griffith <hgriffithdev@gmail.com>
2019-11-10 13:27:01 -0600

    profile is rendering with default user picture and basic info. Added very basic semantic

--- client/components/user-profile.js
+++ client/components/user-profile.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Container, Segment, Grid} from 'semantic-ui-react'
 
-export class Profile extends Component {
+export class UserProfile extends Component {
   render() {
-    return <div>Email: {this.props.user.email}</div>
+    const {user} = this.props
+    return (
+      <Container>
+        <Segment>
+          <Grid columns={2}>
+            <Grid.Column>
+              <img
+                src={user.profilePicture}
+                style={{width: 300, height: 300, marginTop: 30}}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Grid rows={3}>
+                <Grid.Row>{user.email}</Grid.Row>
+                <Grid.Row>{user.firstName}</Grid.Row>
+                <Grid.Row>{user.lastName}</Grid.Row>
+              </Grid>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </Container>
+    )
   }
 }
 
@@ -13,4 +35,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Profile)
+export default connect(mapStateToProps)(UserProfile)
